cc963f3e9c2cdcf4fce23dcc5a3cfe51e4950132
ingyuseo <sig45@naver.com>
2025-06-04 10:23:13 +0900

    [refactor] 37p

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -4,12 +4,10 @@ import java.util.List;
 public class PrimeFactor {
     public List<Integer> of(int number) {
         List<Integer> factors = new ArrayList<>();
-        if(number==2){
-            factors.add(2);
+        if(number>1){
+            factors.add(number);
         }
-        else if(number ==3){
-            factors.add(3);
-        }
+
         return factors;
     }
 }
